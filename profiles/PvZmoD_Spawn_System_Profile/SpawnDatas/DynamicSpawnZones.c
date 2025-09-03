99755bd487f022fde3f1948e4aa48df4673aae15
Christian Miko Tacdol <cmf.tacdol@gmail.com>
2025-09-03 08:36:10 +0800

    save

---
+++ profiles/PvZmoD_Spawn_System_Profile/SpawnDatas/DynamicSpawnZones.c
@@ -0,0 +1,198 @@
+/// !!! Remember that the first zone found has priority on the others (if you have overlapping zones)
+
+/// LOOK AT THE END OF THE FILE FOR MORE HELP !
+/// DYNAMIC SPAWN 		 : 			NUM CONFIG / COORDX-upleft / COORDZ-upleft / COORDX-lowerright / COORDZ-lowerright / QUANTITY RATIO / TOTAL MAX ZEDS NUMBER 
+ref autoptr TIntArray data_Zone001 = {90,			1089,			7184,			1169,				7091,				200,			20}; 		// Castle - Tier 1 Coast
+ref autoptr TIntArray data_Zone002 = {90,			4176,			7416,			4269,				7329,				200,			20}; 		// Castle - Tier 1 Cassandra
+ref autoptr TIntArray data_Zone003 = {90,			4782,			7083,			4899,				6984,				200,			30}; 		// Castle - Tier 1 Southeast of Cassandra
+ref autoptr TIntArray data_Zone004 = {90,			6424,			6566,			6495,				6499,				200,			20}; 		// Castle - West of Toxic
+ref autoptr TIntArray data_Zone005 = {90,			6390,			5950,			6454,				5889,				200,			20}; 		// Castle - Southwest of Toxic
+ref autoptr TIntArray data_Zone006 = {90,			6495,			5563,			6582,				5528,				200,			20}; 		// Castle - Southsouthwest of Toxic
+ref autoptr TIntArray data_Zone007 = {90,			3345,			5377,			3527,				5229,				200,			40}; 		// Old Castle - Centre of map
+ref autoptr TIntArray data_Zone008 = {90,			3860,			4880,			3950,				4797,				200,			20}; 		// Castle - Southeast of Old Castle
+ref autoptr TIntArray data_Zone009 = {30,			2315,			6008,			2831,				5650,				200,			60}; 		// Big Town - Roblido
+ref autoptr TIntArray data_Zone010 = {30,			3925,			7525,			4555,				7274,				400,			90}; 		// Big Town - Cassandra South
+ref autoptr TIntArray data_Zone011 = {30,			4037,			7829,			4741,				7530,				400,			80}; 		// Big Town - Cassandra North
+ref autoptr TIntArray data_Zone012 = {30,			3707,			4858,			4272,				4339,				200,			60}; 		// Big Town - Old Castle town
+ref autoptr TIntArray data_Zone013 = {30,			2036,			3260,			2569,				3023,				200,			60}; 		// Big Town - Ares
+ref autoptr TIntArray data_Zone014 = {30,			6476,			3834,			6952,				3484,				200,			60}; 		// Big Town - Kefka
+ref autoptr TIntArray data_Zone015 = {10,			1146,			7605,			1762,				7363,				100,			15}; 		// Small Town - Tier 1
+ref autoptr TIntArray data_Zone016 = {10,			305,			6336,			620,				6258,				100,			10}; 		// Small Town - Tier 1
+ref autoptr TIntArray data_Zone017 = {10,			648,			6080,			944,				5868,				100,			25}; 		// Small Town - Tier 1
+ref autoptr TIntArray data_Zone018 = {10,			1734,			7666,			2145,				7600,				100,			20}; 		// Small Town - Tier 1
+ref autoptr TIntArray data_Zone019 = {10,			2418,			7669,			2851,				7223,				100,			20}; 		// Small Town - Tier 1
+ref autoptr TIntArray data_Zone020 = {10,			3193,			6530,			3707,				6162,				100,			40}; 		// Small Town - Tier 1
+ref autoptr TIntArray data_Zone021 = {10,			3663,			6684,			4093,				6508,				100,			20}; 		// Small Town - Tier 1
+ref autoptr TIntArray data_Zone022 = {10,			5271,			7220,			5722,				6916,				100,			20}; 		// Small Town - Tier 1
+ref autoptr TIntArray data_Zone023 = {10,			6051,			7127,			6533,				6772,				100,			20}; 		// Small Town - Tier 1
+ref autoptr TIntArray data_Zone024 = {10,			790,			3187,			1398,				2675,				100,			20}; 		// Small Town - Tier 2
+ref autoptr TIntArray data_Zone025 = {94,			2977,			1193,			3105,				1096,				100,			50}; 		// Mixed zombies - Mine entrance
+ref autoptr TIntArray data_Zone026 = {94,			3912,			1760,			4156,				1574,				200,			50}; 		// Small Town - Ruins Tier 2
+ref autoptr TIntArray data_Zone027 = {10,			4779,			731,			5046,				609,				100,			20}; 		// Small Town - Tier 2
+ref autoptr TIntArray data_Zone028 = {10,			5126,			2152,			5529,				1919,				100,			40}; 		// Small Town - Tier 2
+ref autoptr TIntArray data_Zone029 = {95,			4623,			1362,			4910,				1234,				200,			30}; 		// Orchard - Tier 2
+ref autoptr TIntArray data_Zone030 = {1,			6393,			3348,			7511,				3050,				200,			50}; 		// Countryside - Swamp
+ref autoptr TIntArray data_Zone031 = {50,			1393,			5682,			1550,				5583,				200,			10}; 		// Industrial - Tier 1
+ref autoptr TIntArray data_Zone032 = {50,			1830,			5445,			2052,				5383,				200,			10}; 		// Industrial - Tier 1
+ref autoptr TIntArray data_Zone033 = {50,			2818,			6881,			2941,				6751,				200,			10}; 		// Industrial - Tier 1
+ref autoptr TIntArray data_Zone034 = {50,			3388,			6476,			3695,				6181,				200,			20}; 		// Industrial - Tier 1
+/// DYNAMIC SPAWN 		 : 			NUM CONFIG / COORDX-upleft / COORDZ-upleft / COORDX-lowerright / COORDZ-lowerright / QUANTITY RATIO / TOTAL MAX ZEDS NUMBER 
+ref autoptr TIntArray data_Zone035 = {92,			4453,			6609,			4536,				6571,				200,			10}; 		// Industrial - Tier 1
+ref autoptr TIntArray data_Zone036 = {50,			1337,			4504,			1455,				4396,				200,			15}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone037 = {50,			1297,			3615,			1353,				3566,				200,			10}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone038 = {50,			836,			3497,			941,				3385,				200,			15}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone039 = {50,			742,			3388,			842,				3275,				200,			10}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone040 = {50,			1170,			3091,			1289,				3019,				200,			15}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone041 = {92,			1422,			539,			1880,				305,				200,			30}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone042 = {92,			1884,			704,			2226,				331,				200,			30}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone043 = {92,			2007,			867,			2276,				628,				200,			50}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone044 = {50,			2986,			894,			3132,				793,				200,			20}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone045 = {50,			7655,			1981,			7838,				1860,				200,			20}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone046 = {10,			7424,			5486,			7633,				5274,				100,			20}; 		// Small Town - Tier 2
+ref autoptr TIntArray data_Zone047 = {50,			6148,			7145,			6385,				7053,				100,			10}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone048 = {50,			5556,			7243,			5652,				7122,				100,			10}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone049 = {50,			4045,			7850,			4523,				7781,				100,			10}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone050 = {50,			4434,			7977,			4735,				7839,				100,			20}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone051 = {50,			3653,			6046,			3871,				5944,				100,			15}; 		// Industrial - Tier 2
+ref autoptr TIntArray data_Zone052 = {60,			1728,			6570,			1831,				6458,				100,			20}; 		// Military - Tier 1
+ref autoptr TIntArray data_Zone053 = {60,			835,			3529,			913,				3384,				100,			20}; 		// Military - Tier 2
+ref autoptr TIntArray data_Zone054 = {60,			1584,			2637,			1712,				2572,				200,			50}; 		// Military - Tier 2
+ref autoptr TIntArray data_Zone055 = {60,			5847,			1925,			7794,				1748,				400,			80}; 		// Military - Airfield S
+ref autoptr TIntArray data_Zone056 = {60,			6474,			2369,			7785,				2004,				400,			100}; 		// Military - Airfield N
+ref autoptr TIntArray data_Zone057 = {60,			7376,			1683,			7695,				1421,				400,			50}; 		// Military - Airfield Base
+ref autoptr TIntArray data_Zone058 = {60,			5927,			4822,			5982,				4773,				500,			40}; 		// Military - Tunnel W
+ref autoptr TIntArray data_Zone059 = {60,			6305,			4042,			6316,				4060,				500,			40}; 		// Military - Tunnel E
+ref autoptr TIntArray data_Zone060 = {60,			6912,			6175,			7295,				6058,				100,			30}; 		// Military - Tier 2
+ref autoptr TIntArray data_Zone061 = {60,			6776,			7212,			7122,				6496,				100,			100}; 		// Military - Tier 2 Toxic
+ref autoptr TIntArray data_Zone062 = {60,			7537,			7363,			7683,				7158,				100,			20}; 		// Military - Tier 2
+ref autoptr TIntArray data_Zone063 = {60,			5220,			5100,			5428,				5004,				100,			20}; 		// Military - Tier 2
+ref autoptr TIntArray data_Zone064 = {60,			5484,			4155,			5569,				4022,				100,			20}; 		// Military - Tier 2
+ref autoptr TIntArray data_Zone065 = {92,			4998,			3707,			5165,				3565,				100,			20}; 		// Military - Tier 2
+ref autoptr TIntArray data_Zone066 = {92,			2580,			4398,			2820,				4197,				100,			20}; 		// Military - Tier 2
+ref autoptr TIntArray data_Zone067 = {99,			7218,			7110,			7900,				6391,				100,			60}; 		// Toxic Zone
+ref autoptr TIntArray data_Zone068 = {30,			4942,			4973,			4916,				4929,				100,			10}; 		// Bus Crash
+ref autoptr TIntArray data_Zone069 = {94,			2337,			2082,			2611,				2108,				100,			80}; 		// Quarry Floor
+ref autoptr TIntArray data_Zone070 = {60,			3197,			6199,			3300,				6135,				100,			30}; 		// Military - South of Cassandra
+ref autoptr TIntArray data_Zone071 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone072 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone073 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone074 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+/// DYNAMIC SPAWN 		 : 			NUM CONFIG / COORDX-upleft / COORDZ-upleft / COORDX-lowerright / COORDZ-lowerright / QUANTITY RATIO / TOTAL MAX ZEDS NUMBER 
+ref autoptr TIntArray data_Zone075 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone076 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone077 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone078 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone079 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone080 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone081 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone082 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone083 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone084 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone085 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone086 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone087 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone088 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone089 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone090 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone091 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone092 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone093 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone094 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone095 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone096 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone097 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone098 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone099 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone100 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone101 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+/// DYNAMIC SPAWN 		 : 			NUM CONFIG / COORDX-upleft / COORDZ-upleft / COORDX-lowerright / COORDZ-lowerright / QUANTITY RATIO / TOTAL MAX ZEDS NUMBER 
+ref autoptr TIntArray data_Zone102 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone103 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone104 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone105 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone106 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone107 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone108 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone109 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone110 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone111 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone112 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone113 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone114 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone115 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone116 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone117 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone118 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone119 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+
+/// !!! Remember that the first zone found has priority on the others (if you have overlapping zones)
+
+/// You can use these lines to add new Dynamic zones 
+/// (I highly recommend to you to use the last lines first because I will surely add new zones and take the first for more readability)
+
+/// DYNAMIC SPAWN 		 : 			NUM CONFIG / COORDX-upleft / COORDZ-upleft / COORDX-lowerright / COORDZ-lowerright / QUANTITY RATIO / TOTAL MAX ZEDS NUMBER 
+ref autoptr TIntArray data_Zone120 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone121 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone122 = {0,			0,				0,				0,					0,					100,			100}; 		// 
+ref autoptr TIntArray data_Zone123 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone124 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone125 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone126 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone127 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone128 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone129 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone130 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone131 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone132 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone133 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone134 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone135 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone136 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone137 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone138 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone139 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone140 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone141 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone142 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone143 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone144 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone145 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone146 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone147 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone148 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone149 = {0,			0,				0,				0,					0,					100,			100}; 					//
+ref autoptr TIntArray data_Zone150 = {0,			0,				0,				0,					0,					100,			100}; 					//
+/// DYNAMIC SPAWN 		 : 			NUM CONFIG / COORDX-upleft / COORDZ-upleft / COORDX-lowerright / COORDZ-lowerright / QUANTITY RATIO / TOTAL MAX ZEDS NUMBER 
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// !!! Remember that the first zone found has priority on the others (if you have overlapping zones)
+
+/// If you want to deactivate Dynamic spawns properly (and save server ressources) put 0 in NUM CONFIG column, for example :
+/// ref autoptr TIntArray data_Zone002 = {0,		828,			7775,			1148,				7455,				100,			100}; // => correctly disable
+/// This will not create a "safe zone" without Z but will be replaced by countryside z data
+/// If you want to make a safe zone without Zeds put 0 to QUANTITY RATIO
+/// ref autoptr TIntArray data_Zone002 = {50,		828,			7775,			1148,				7455,				0,				100}; // => Safe zone
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// NUM CONFIG		: Configuration Number (number of zeds,	time to spawn...) look at ZombiesDynamicSpawnDatas.c file
+
+/// COORDX-upleft	: X coordinate of the upper left corner of the zone
+/// COORDZ-upleft	: Z coordinate of the upper left corner of the zone
+
+/// COORDX-lowerright	: X coordinate of the lower right corner of the zone
+/// COORDZ-lowerright	: Z coordinate of the lower right corner of the zone
+
+/// QUANTITY RATIO : Increase or decrease (100 is default) the ratio (%) of zeds to spawn (NBRmini/NBRmaxi in ZombiesDynamicSpawnDatas.c) 
+/// for example if I want Electro to have twice more zeds than other big cities, I set 200 to Electro line.
+/// If I want to make a "safe zone" without Zeds => 0
+
+/// TOTAL MAX ZEDS NUMBER : The maximum number of simultaneous zombies in the area.
+/// This include dead bodies but not the static spawns. 
+/// This allow to clean a zone if the dead bodies don't despawn too fast (value "19" in .json) 
+/// This avoid infinite waves if the dead bodies don't despawn too close to the players (value "18" in .json) 
+
+
+
+
